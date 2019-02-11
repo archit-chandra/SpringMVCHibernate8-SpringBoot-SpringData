cbe1962986f308a2e497af7d27dc5922aeafb64d
archit_chandra <send2archit.chandra@gmail.com>
2019-02-11 22:57:18 +0100

    added extra endpoints to be accessed (to test automatic reloading)

--- 02-dev-tools-demo/src/main/java/com/example/devtoolsdemo/rest/FunRestController.java
+++ 02-dev-tools-demo/src/main/java/com/example/devtoolsdemo/rest/FunRestController.java
@@ -14,4 +14,15 @@ public class FunRestController {
         return "Hello World! Time on the server is: " + LocalDate.now();
     }
 
+    // expose a new endpoint for "/workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run 5K daily for a month";
+    }
+
+    // expose a new endpoint for "/fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day!";
+    }
 }
