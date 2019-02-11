7218b26025ae7099e852d6c1225eeb3065c29130
archit_chandra <send2archit.chandra@gmail.com>
2019-02-11 21:57:27 +0100

    added first demo spring boot app: 'MyCoolApp' - with essential files

---
+++ 1.MyCoolApp/src/main/java/com/example/mycoolapp/MyCoolAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoolAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCoolAppApplication.class, args);
+    }
+
+}
+
