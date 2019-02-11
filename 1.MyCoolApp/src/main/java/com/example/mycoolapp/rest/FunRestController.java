16043317da208e92ceaea44ced5b184acdd0ba2d
archit_chandra <send2archit.chandra@gmail.com>
2019-02-11 22:07:16 +0100

    created a controller to avoid "Whitelabel Error Page"

---
+++ 1.MyCoolApp/src/main/java/com/example/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,16 @@
+package com.example.mycoolapp.rest;
+
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World! Time on the server is: " + LocalDate.now();
+    }
+}
