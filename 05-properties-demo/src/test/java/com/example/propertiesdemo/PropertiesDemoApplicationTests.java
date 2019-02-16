a3f5a0c0dcb138ea19c6b021ad948f3df2b0b2ea
archit_chandra <send2archit.chandra@gmail.com>
2019-02-17 00:33:53 +0100

    created project 'properties-demo'

---
+++ 05-properties-demo/src/test/java/com/example/propertiesdemo/PropertiesDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.propertiesdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PropertiesDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
