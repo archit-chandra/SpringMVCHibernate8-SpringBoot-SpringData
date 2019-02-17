23f9c639ad966c4a277250c82a94b262ecd6ddef
archit_chandra <send2archit.chandra@gmail.com>
2019-02-17 07:16:02 +0100

    added other CRUD operations in REST controller (3 TODOs left)

--- 06-springboot-rest-hibernate/src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
+++ 06-springboot-rest-hibernate/src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
@@ -3,7 +3,12 @@ package com.example.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +29,40 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.getEmployeeById(employeeId);
+        if (employee == null) {
+            // TODO: throw a custom exception with error response
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return employeeService.getEmployeeById(employeeId);
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        // TODO: check whether the employee exists or not. Throw an exception, if not
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.getEmployeeById(employeeId);
+        if (employee == null) {
+            // TODO: throw a custom exception with error response
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        employeeService.deleteEmployeeById(employeeId);
+        return "Deleted employee id - " + employeeId;
     }
 }
